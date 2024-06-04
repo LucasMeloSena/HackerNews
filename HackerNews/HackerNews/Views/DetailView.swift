8c20e7c9580054bf0897d5af963dacb84f1d212f
Lucas Sena <93053816+LucasMeloSena@users.noreply.github.com>
2024-06-04 17:23:00 -0300

    Add files via upload

---
+++ HackerNews/HackerNews/Views/DetailView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+#Preview {
+    DetailView(url: "")
+}
