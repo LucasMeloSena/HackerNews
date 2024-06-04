e147b410e110d3199d8e5874c8ead00568b22039
Lucas Sena <93053816+LucasMeloSena@users.noreply.github.com>
2024-06-04 17:37:56 -0300

    Add files via upload

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Lucas Sena on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackerNews/Views/WebView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeUrl = urlString {
+            if let url = URL(string: safeUrl) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
